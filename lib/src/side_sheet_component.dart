b0229eca554f87bf80ea564777ebdea2d8a76c0b
Israel Hudson <israelhudson@gmail.com>
2022-08-31 00:57:04 -0300

    document methods

--- lib/src/side_sheet_component.dart
+++ lib/src/side_sheet_component.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
 class SideSheet {
+  /// Open Left side sheet
+  /// ```dart
+  ///onPressed: () => SideSheet.left(body: Text("Body"), context: context)
+  /// ```
   static Future<dynamic> left(
-      {required Widget body,
+      {
+
+      ///Use this to pass any widget you want to display in the side sheet
+      required Widget body,
       required BuildContext context,
+
+      /// Use this to set the width of the side sheet
       double? width,
       String barrierLabel: "Side Sheet",
+
+      /// Use barrierDismissible to dismiss the side sheet by tapping outside of it
       bool barrierDismissible: true,
+
+      /// Use barrierColor to set the outside color of the side sheet when opened
       Color barrierColor = const Color(0xFF66000000),
+
+      /// Use transitionDuration to set the duration of the animation when the side sheet is opened or closed
       Duration transitionDuration = const Duration(milliseconds: 300)}) async {
     dynamic data = await _showSheetSide(
         body: body,
@@ -23,13 +38,28 @@ class SideSheet {
     return data;
   }
 
+  /// Open Right side sheet
+  /// ```dart
+  ///onPressed: () => SideSheet.right(body: Text("Body"), context: context)
+  /// ```
   static Future<dynamic> right(
-      {required Widget body,
+      {
+
+      ///Use this to pass any widget you want to display in the side sheet
+      required Widget body,
       required BuildContext context,
+
+      /// Use this to set the width of the side sheet
       double? width,
       String barrierLabel: "Side Sheet",
+
+      /// Use barrierDismissible to dismiss the side sheet by tapping outside of it
       bool barrierDismissible: true,
+
+      /// Use barrierColor to set the outside color of the side sheet when opened
       Color barrierColor = const Color(0xFF66000000),
+
+      /// Use transitionDuration to set the duration of the animation when the side sheet is opened or closed
       Duration transitionDuration = const Duration(milliseconds: 300)}) async {
     dynamic data = await _showSheetSide(
         body: body,
